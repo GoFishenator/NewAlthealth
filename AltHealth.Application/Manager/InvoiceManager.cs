d801f57b9d7542130394e9c72f59d38b8c862183
Reinhardt Wittstock <wittstockr@fourierit.co.za>
2019-11-04 10:11:46 +0200

    Updated Invoice Manager and added InvoiceDisplayDto

--- AltHealth.Application/Manager/InvoiceManager.cs
+++ AltHealth.Application/Manager/InvoiceManager.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AltHealth.Application.MappingProfiles;
+using AltHealth.Application.Models;
 using AltHealth.Data;
 using AltHealth.Data.Repository;
 using AutoMapper;
@@ -17,16 +18,10 @@ namespace AltHealth.Application.Manager
         private readonly DataRepository<InvoiceBooking> _dbInvoiceBooking = new DataRepository<InvoiceBooking>();
         private readonly DataRepository<InvoiceItem> _dbInvoiceItem = new DataRepository<InvoiceItem>();
         private readonly DataRepository<Patient> _dbPatient = new DataRepository<Patient>();
-        private readonly IMapper _mapper;
 
         public InvoiceManager()
         {
-            var mappingConfig = new MapperConfiguration(mc =>
-            {
-                mc.AddProfile(new MappingProfile());
-            });
-
-            _mapper = mappingConfig.CreateMapper();
+            
         }
 
         public void CreateInvoice(int bookingId)
@@ -41,13 +36,112 @@ namespace AltHealth.Application.Manager
                     var invoice = new Invoice()
                     {
                         InvoiceNumber = patient.Id + "0" + random.Next(10000) + DateTime.Now.Day + DateTime.Now.Month + DateTime.Now.Year,
-                        TotalAmount = 0
+                        TotalAmount = 0m
                     };
                     _dbInvoice.Add(invoice);
-
+                    _dbInvoice.Save();
+                    var newInvoice = _dbInvoice.GetAll().OrderByDescending(x => x.Id).First();
+                    var bookingInvoice = new InvoiceBooking()
+                    {
+                        BookingId = bookingId,
+                        InvoiceId = newInvoice.Id,
+                        InvoiceDate = DateTime.Now,
+                        IsPaid = false,
+                        IsSent = false,
+                        PayedUpDate = null,
+                        OutStandingAmount = 0m
+                    };
+                    _dbInvoiceBooking.Add(bookingInvoice);
+                    _dbInvoiceBooking.Save();
                 }
                 
             }
+        }
+
+        public List<InvoiceDisplayDto> GetAllInvoices()
+        {
+            var invoiceBookings = _dbInvoiceBooking.GetAll();
+
+            var result = new List<InvoiceDisplayDto>();
+            foreach (var item in invoiceBookings)
+            {
+                var booking = _dbBooking.GetAll().First(x => x.Id == item.BookingId);
+                var invoice = _dbInvoice.GetAll().First(x => x.Id == item.InvoiceId);
+                var invoiceDisplay = new InvoiceDisplayDto()
+                {
+                    BookingId = item.BookingId,
+                    InvoiceId = item.InvoiceId,
+                    InvoiceDate = item.InvoiceDate,
+                    IsPaid = item.IsPaid,
+                    IsSent = item.IsSent,
+                    PayedUpDate = item.PayedUpDate,
+                    OutStandingAmount = item.OutStandingAmount,
+                    BookingNumber = booking.BookingNumber,
+                    InvoiceNumber = invoice.InvoiceNumber,
+                    TotalAmount = invoice.TotalAmount,
+                    InvoiceItems = new List<InvoiceItem>()
+                };
+                invoiceDisplay.InvoiceItems = _dbInvoiceItem.GetAll().Where(x => x.InvoiceId == item.InvoiceId).ToList();
+                result.Add(invoiceDisplay);
+            }
+
+
+            return result;
+        }
+
+        public InvoiceDisplayDto GetInvoice(int id)
+        {
+            var item = _dbInvoiceBooking.GetAll().First(x => x.Id == id);
+            var booking = _dbBooking.GetAll().First(x => x.Id == item.BookingId);
+            var invoice = _dbInvoice.GetAll().First(x => x.Id == item.InvoiceId);
+            var invoiceDisplay = new InvoiceDisplayDto()
+            {
+                BookingId = item.BookingId,
+                InvoiceId = item.InvoiceId,
+                InvoiceDate = item.InvoiceDate,
+                IsPaid = item.IsPaid,
+                IsSent = item.IsSent,
+                PayedUpDate = item.PayedUpDate,
+                OutStandingAmount = item.OutStandingAmount,
+                BookingNumber = booking.BookingNumber,
+                InvoiceNumber = invoice.InvoiceNumber,
+                TotalAmount = invoice.TotalAmount,
+                InvoiceItems = new List<InvoiceItem>()
+            };
+            invoiceDisplay.InvoiceItems = _dbInvoiceItem.GetAll().Where(x => x.InvoiceId == item.InvoiceId).ToList();
+            return invoiceDisplay;
+        }
+
+        public void AddInvoiceItem(int invoiceId, InvoiceItemDto item)
+        {
+            var invoiceItem = new InvoiceItem()
+            {
+                Name = item.Name,
+                Description = item.Description,
+                Amount = item.Amount,
+                Quantity = item.Quantity,
+                InvoiceId = invoiceId
+            };
+            _dbInvoiceItem.Add(invoiceItem);
+            _dbInvoiceItem.Save();
+        }
+
+        public void UpdateInvoiceItem(InvoiceItemDto item)
+        {
+            var invoiceItem = _dbInvoiceItem.GetAll().First(x => x.Id == item.Id);
+            invoiceItem.Id = item.Id;
+            invoiceItem.Name = item.Name;
+            invoiceItem.Description = item.Description;
+            invoiceItem.Amount = item.Amount;
+            invoiceItem.Quantity = item.Quantity;
+            invoiceItem.InvoiceId = item.InvoiceId;
+            _dbInvoiceItem.Edit(invoiceItem);
+        }
+
+        public void RemoveInvoiceItem(int id)
+        {
+            var invoiceItem = _dbInvoiceItem.GetAll().First(x => x.Id == id);
+            _dbInvoiceItem.Delete(invoiceItem);
         }
     }
 }
---
+++ AltHealth.Application/Models/InvoiceDisplayDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AltHealth.Data;
+
+namespace AltHealth.Application.Models
+{
+    public class InvoiceDisplayDto: InvoiceBookingDto
+    {
+        public string BookingNumber { get; set; }
+        public string InvoiceNumber { get; set; }
+        public decimal TotalAmount { get; set; }
+        public List<InvoiceItem> InvoiceItems { get; set; }
+    }
+}
